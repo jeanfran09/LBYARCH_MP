22c06bd169aee4e92933e09c30b8d95cb9f293d5
drea-na <andreae.loria@gmail.com>
2024-11-26 15:26:08 +0800

    update main.c
    - Added timer
    - Added dynamic array (not sure about this, how to use malloc in 2d array, will search pa)
    - Added sanity check

--- lbyarch_mp/main.c
+++ lbyarch_mp/main.c
@@ -4,18 +4,72 @@
 
 extern void x64calc(long long int n, float* arr[][3]);
 
+void getAcceleration(long long int n, float* arr[][3], float* x) {
+	int i;
+	float delta_v, acceleration;
+
+	for (i = 0; i < n; i++) {
+		delta_v = (arr[i][1] - arr[i][0]) * 1000.0 / 3600.0;
+		acceleration = delta_v / arr[i][2];
+
+		x = acceleration; 
+
+		printf("[Row %d] a = %.2f m/s^2\n", i + 1, acceleration);
+	}
+}
+
 int main() {
+	unsigned long long int ARRAY_SIZE = 3;
+	unsigned long long int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
+	clock_t start, end;
+	double time_taken;
+	int i;
 
 	float arr[][3] = { {0.0, 62.5, 10.1}, {60.0, 122.3, 5.5},{30.0, 160.7, 7.8} };
+
+	//declare array to store the acceleration per row
+	float* x;
+	x = (float*)malloc(ARRAY_BYTES);
 	
+	for (i = 0; i < ARRAY_SIZE; i++) {
+		x[i] = 0.0; 
+	}
+
+	getAcceleration(ARRAY_SIZE, arr[][3]);
+
+	/*
 	int i = 0;
 	/*for (i = 0; i < 3; i++) {
 		printf("%f\n", arr[1][i]);
-	}*/
+	}
 	x64calc(3, arr);
 	for (i = 0; i < 3; i++) {
 		printf("%f\n", arr[i][2]);//final answer is stored in arr[i][2] might change later
 	}
+	*/
+
+	//C 
+	//------------------------
+	start = clock();
+	getAcceleration(ARRAY_SIZE, arr);
+	end = clock();
+	time_taken = ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
+
+	//sanity check
+	//print the values of x
+	for (i = 0; i < ARRAY_SIZE; i++) {
+		printf("[%d] Acceleration = %lf", i + 1, x[i]);
+	}
+
+	//x86-64
+	//------------------------
+	//reinitialize the array
+	float arr[][3] = { {0.0, 62.5, 10.1}, {60.0, 122.3, 5.5},{30.0, 160.7, 7.8} };
+
+	start = clock();
+	x64calc(3, arr);
+	end = clock();
+	printf("%lf ms\,", time_taken);
 
 	return 0;
 }
