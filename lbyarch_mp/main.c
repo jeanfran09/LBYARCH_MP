939629a04086d3cf11393020ec9732860b3f0626
drea-na <andreae.loria@gmail.com>
2024-11-30 10:07:02 +0800

    Update main.c
    - add malloc for 2d array
    - initialize array using rand()
    - vi random float from 0 to 50
    - vf random float from 50.0 to 100.0
    - time random float from 5.0 to 10.0
    
    (not sure about the range ^ will double check)

--- lbyarch_mp/main.c
+++ lbyarch_mp/main.c
@@ -2,78 +2,62 @@
 #include <stdlib.h>
 #include <time.h>
 
-extern void x64calc(long long int n, float* arr[][3]);
+extern void x64calc(long long int n, float arr[][3]);
 
-void getAcceleration(long long int n, float arr[][3], float* x) {
-	int i;
-	float delta_v, acceleration;
+void getAcceleration(long long int n, float arr[][3]) {
+    int i;
+    float delta_v, acceleration;
 
-	for (i = 0; i < n; i++) {
-		delta_v = (arr[i][1] - arr[i][0]) * 1000 / 3600;
-		acceleration = delta_v / arr[i][2];
+    for (i = 0; i < n; i++) {
+        delta_v = (arr[i][1] - arr[i][0]) * 1000 / 3600;
+        acceleration = delta_v / arr[i][2];
 
-		x[i] = acceleration;
-
-		printf("[Row %d] a = %.2f m/s^2\n", i + 1, acceleration);
-	}
+        printf("[Row %d] vf = %.2f | vi = %.2f | t = %.2f | a = %.2f m/s^2\n",
+            i + 1, arr[i][1], arr[i][0], arr[i][2], acceleration);
+    }
 }
 
 int main() {
-	unsigned long long int ARRAY_SIZE = 3;
-	unsigned long long int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
-	clock_t start, end;
-	double time_taken;
-	int i;
+    unsigned long long int ARRAY_SIZE = 100;
+    size_t ARRAY_BYTES = ARRAY_SIZE * 3 * sizeof(float);
+    clock_t start, end;
+    double time_taken;
+    int i;
 
-	float arr[][3] = { {0.0, 62.5, 10.1}, {60.0, 122.3, 5.5},{30.0, 160.7, 7.8} };
+    float (*arr)[3] = malloc(ARRAY_BYTES);
 
-	//declare array to store the acceleration per row
-	float* x;
-	x = (float*)malloc(ARRAY_BYTES);
-	
-	for (i = 0; i < ARRAY_SIZE; i++) {
-		x[i] = 0.0; 
-	}
+    srand(time(NULL));
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        arr[i][0] = (float)rand() / (RAND_MAX / 50.0f);
+        arr[i][1] = 50.0f + (float)rand() / (RAND_MAX / 50.0f);
+        arr[i][2] = 5.0f + (float)rand() / (RAND_MAX / 10.0f);
 
-	//getAcceleration(ARRAY_SIZE, arr, x);
+    }
 
-	/*
-	int i = 0;
-	/*for (i = 0; i < 3; i++) {
-		printf("%f\n", arr[1][i]);
-	}
-	x64calc(3, arr);
-	for (i = 0; i < 3; i++) {
-		printf("%f\n", arr[i][2]);//final answer is stored in arr[i][2] might change later
-	}
-	*/
+    // C Execution
+    //------------------------
+    printf("\nC Execution\n");
+    start = clock();
+    getAcceleration(ARRAY_SIZE, arr);
+    end = clock();
+    time_taken = ((double)(end - start) * 10000 / CLOCKS_PER_SEC);
+    printf("[C] Time: %lf ms\n", time_taken);
 
-	//C 
-	//------------------------
-	start = clock();
-	getAcceleration(ARRAY_SIZE, arr, x);
-	end = clock();
-	time_taken = ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
+    // x86-64 Execution
+    //------------------------
+    printf("\nx86-64 Execution\n");
+    start = clock();
+    x64calc(ARRAY_SIZE, arr);
+    
+    end = clock();
+    time_taken = ((double)(end - start) * 10000 / CLOCKS_PER_SEC);
 
-	printf("%lf ms\,\n", time_taken);
-	//sanity check
-	//print the values of x
-	for (i = 0; i < ARRAY_SIZE; i++) {
-		printf("[%d] Acceleration = %lf\n", i + 1, x[i]);
-	}
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        printf("[Row %d] Final answer: %.2f\n", i + 1, arr[i][2]);
+    }
+    printf("[x86-64] Time: %lf ms\n", time_taken);
 
-	//x86-64
-	//------------------------
-	//reinitialize the array
-	//float arr[][3] = {{0.0, 62.5, 10.1}, {60.0, 122.3, 5.5},{30.0, 160.7, 7.8}};
+    
 
-	start = clock();
-	x64calc(3, arr);
-	end = clock();
-	time_taken = ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
-	printf("%lf ms\,\n", time_taken);
-	for (i = 0; i < 3; i++) {
-		printf("%f\n", arr[i][2]);//final answer is stored in arr[i][2] might change later
-	}
-	return 0;
+    return 0;
 }
