7b5378a439b60af502791a6bfb868c51adeb782b
jeanfran09 <jeanfran0909@gmail.com>
2024-11-30 10:40:07 +0800

    transferred printf statements and added array to store answer for c

--- lbyarch_mp/main.c
+++ lbyarch_mp/main.c
@@ -4,21 +4,21 @@
 
 extern void x64calc(long long int n, float arr[][3]);
 
-void getAcceleration(long long int n, float arr[][3]) {
+void getAcceleration(long long int n, float arr[][3], float* ans) {
     int i;
-    float delta_v, acceleration;
+    float delta_v/*, acceleration*/;
 
     for (i = 0; i < n; i++) {
         delta_v = (arr[i][1] - arr[i][0]) * 1000 / 3600;
-        acceleration = delta_v / arr[i][2];
+        ans[i] = delta_v / arr[i][2];
 
-        printf("[Row %d] vf = %.2f | vi = %.2f | t = %.2f | a = %.2f m/s^2\n",
-            i + 1, arr[i][1], arr[i][0], arr[i][2], acceleration);
+        /*printf("[Row %d] vf = %.2f | vi = %.2f | t = %.2f | a = %.2f m/s^2\n",
+            i + 1, arr[i][1], arr[i][0], arr[i][2], acceleration);*/
     }
 }
 
 int main() {
-    unsigned long long int ARRAY_SIZE = 100;
+    unsigned long long int ARRAY_SIZE = 100000;
     size_t ARRAY_BYTES = ARRAY_SIZE * 3 * sizeof(float);
     clock_t start, end;
     double time_taken;
@@ -26,34 +26,41 @@ int main() {
 
     float (*arr)[3] = malloc(ARRAY_BYTES);
 
+    float* ans = (float*)malloc(ARRAY_SIZE * sizeof(float));
+
     srand(time(NULL));
-    for (i = 0; i < ARRAY_SIZE; i++) {
+    for (i = 0; i < 10; i++) {
         arr[i][0] = (float)rand() / (RAND_MAX / 50.0f);
         arr[i][1] = 50.0f + (float)rand() / (RAND_MAX / 50.0f);
         arr[i][2] = 5.0f + (float)rand() / (RAND_MAX / 10.0f);
-
     }
+
 
     // C Execution
     //------------------------
     printf("\nC Execution\n");
     start = clock();
-    getAcceleration(ARRAY_SIZE, arr);
+    getAcceleration(ARRAY_SIZE, arr, ans);
     end = clock();
     time_taken = ((double)(end - start) * 10000 / CLOCKS_PER_SEC);
+    for (i = 0; i < 10; i++) {
+        printf("[Row %d] vf = %.2f | vi = %.2f | t = %.2f | a = %.2f m/s^2\n",
+            i + 1, arr[i][1], arr[i][0], arr[i][2], ans[i]);
+    }
+
     printf("[C] Time: %lf ms\n", time_taken);
+
 
     // x86-64 Execution
     //------------------------
     printf("\nx86-64 Execution\n");
     start = clock();
     x64calc(ARRAY_SIZE, arr);
-    
     end = clock();
     time_taken = ((double)(end - start) * 10000 / CLOCKS_PER_SEC);
 
-    for (i = 0; i < ARRAY_SIZE; i++) {
-        printf("[Row %d] Final answer: %.2f\n", i + 1, arr[i][2]);
+    for (i = 0; i < 10; i++) {
+        printf("[Row %d] Final answer: %.2f m/s^2\n", i + 1, arr[i][2]);
     }
     printf("[x86-64] Time: %lf ms\n", time_taken);
 
