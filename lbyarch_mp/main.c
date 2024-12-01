0d58b79c33e60b556501abbaf5836836a05d1408
drea-na <andreae.loria@gmail.com>
2024-12-01 21:32:37 +0800

    Update main.c
    - cleaned the file removed unnecessary comments
    - removed redundancy, placed redundant code in main

--- lbyarch_mp/main.c
+++ lbyarch_mp/main.c
@@ -24,9 +24,9 @@ void cmpArr(long long int n, float* c, int* x64) {
 
     for (i = 0; i < n; i++) {
         int roundedC;
-        float fractional_part = c[i] - floorf(c[i]);  
+        float fraction = c[i] - floorf(c[i]);  
 
-        if (fractional_part == 0.5f) {
+        if (fraction == 0.5f) {
             roundedC = (int)(roundf(c[i] / 2) * 2);
         }
         else {
@@ -42,177 +42,78 @@ void cmpArr(long long int n, float* c, int* x64) {
     }
 
     if (error == 1) {
-        printf("There are errors in some of the acceleration values.\n");
+        printf("There are errors in the acceleration values.\n");
     }
     else {
         printf("All acceleration values are correct!\n");
     }
 }
 
-void getTxt(const char* filename) {
-    unsigned long long int array_size;
+void getTxt(const char* filename, unsigned long long int* array_size, float (**arr)[3]) {
+    //https://www.geeksforgeeks.org/c-program-to-read-contents-of-whole-file/
     FILE* ptr = fopen(filename, "r");
     if (ptr == NULL) {
-        printf("Error: Could not open file %s. Check if the file exists and the path is correct.\n", filename);
+        printf("Cannot open file %s.\n", filename);
         exit(1);
     }
 
-    if (fscanf_s(ptr, "%llu", &array_size) != 1) {
-        printf("Error: Failed to read array size from the file.\n");
+    if (fscanf_s(ptr, "%llu", array_size) != 1) {
+        printf("Read failure\n");
         fclose(ptr);
         exit(1);
     }
 
     printf("File: %s\n", filename);
-    printf("Array size: %llu\n", array_size);
+    printf("Array size: %llu\n", *array_size);
 
-    float (*arr)[3] = malloc(array_size * 3 * sizeof(float));
+    *arr = malloc((*array_size) * 3 * sizeof(float));
     if (*arr == NULL) {
-        printf("Error: Memory allocation failed.\n");
+        printf("Malloc failure\n");
         fclose(ptr);
         exit(1);
     }
 
-    for (unsigned long long i = 0; i < array_size; i++) {
-        if (arr[i] == NULL) {
-            printf("Error: Memory allocation failed for row %llu.\n", i);
+    for (unsigned long long i = 0; i < *array_size; i++) {
+        if (fscanf_s(ptr, "%f %f %f", &(*arr)[i][0], &(*arr)[i][1], &(*arr)[i][2]) != 3) {
+            printf("Invalid input\n");
             fclose(ptr);
-            exit(1);
-        }
-    }
-
-    for (unsigned long long i = 0; i < array_size; i++) {
-        if (fscanf_s(ptr, "%f %f %f", &arr[i][0], &arr[i][1], &arr[i][2]) != 3) {
-            printf("Error: Incorrect file format or insufficient data.\n");
-            fclose(ptr);
+            free(*arr);
             exit(1);
         }
     }
 
     fclose(ptr);
-    printf("Successfully read %llu rows from %s\n", array_size, filename);
-
-    float* ansC = (float*)malloc(array_size * sizeof(float));
-    int* ans64 = (int*)malloc(array_size * sizeof(float));
-    LARGE_INTEGER start, end, frequency;
-    double time_taken, total_time = 0.0;
-    double query_total = 0.0, query_avg;
-
-    getAcceleration(array_size, arr, ansC);
-
-    QueryPerformanceFrequency(&frequency);
-
-    for (int i = 0; i < 30; i++) {
-        QueryPerformanceCounter(&start);
-
-        x64calc(array_size, arr, ans64);
-
-        QueryPerformanceCounter(&end);
-
-        time_taken = ((double)(end.QuadPart - start.QuadPart) * 1000000.0) / frequency.QuadPart;
-        total_time += time_taken;
-    }
-
-    query_total = total_time;
-    query_avg = total_time / 30;
-
-    // Printing the results of the acceleration calculations
-    for (unsigned long long j = 0; j < array_size; j++) {
-        printf("[%llu] acceleration = %d\n", j, ans64[j]);
-    }
-
-    printf("\n----------------------------------------\n");
-    printf("\nRESULTS \n\n");
-    printf("Number of rows: %llu\n", array_size);
-    printf("Correctness check: \n");
-    cmpArr(array_size, ansC, ans64);
-
-    printf("\nQuery Performance\n");
-    printf("Average time per query (30 runs): %.2f 탎\n", query_avg);
-    printf("Total query time for 30 runs: %.2f 탎\n", query_total);
-
-    printf("\n----------------------------------------\n");
+    printf("Successfully read %llu rows from %s\n", *array_size, filename);
 }
 
-
-void getInput() {
-    unsigned long long int array_size;
+void getInput(unsigned long long int* array_size, float (**arr)[3]) {
     printf("Enter the number of rows: ");
-    if (scanf_s("%llu", &array_size) != 1) {
-        printf("Error: Invalid input.\n");
+    if (scanf_s("%llu", array_size) != 1) {
+        printf("Invalid input.\n");
         exit(1);
     }
 
-    float (*arr)[3] = malloc(array_size * 3 * sizeof(float));
-
+    *arr = malloc(*array_size * sizeof(float[3]));
     if (*arr == NULL) {
-        printf("Error: Memory allocation failed.\n");
+        printf("Malloc failure\n");
         exit(1);
-    }
-
-    for (unsigned long long i = 0; i < array_size; i++) {
-        //(*arr)[i] = (float*)malloc(3 * sizeof(float));
-        if (arr[i] == NULL) {
-            printf("Error: Memory allocation failed for row %llu.\n", i);
-            exit(1);
-        }
     }
 
     printf("Enter the values (vi vf t) for each row:\n");
-    for (unsigned long long i = 0; i < array_size; i++) {
+    for (unsigned long long i = 0; i < *array_size; i++) {
         printf("Row %llu: ", i + 1);
-        if (scanf_s("%f %f %f", &arr[i][0], &arr[i][1], &arr[i][2]) != 3) {
-            printf("Error: Invalid input.\n");
+        if (scanf_s("%f %f %f", &(*arr)[i][0], &(*arr)[i][1], &(*arr)[i][2]) != 3) {
+            printf("Invalid input.\n");
+            free(*arr); 
             exit(1);
         }
     }
-
-    float* ansC = (float*)malloc(array_size * sizeof(float));
-    int* ans64 = (int*)malloc(array_size * sizeof(float));
-    LARGE_INTEGER start, end, frequency;
-    double time_taken, total_time = 0.0;
-    double query_total = 0.0, query_avg;
-
-    getAcceleration(array_size, arr, ansC);
-
-    QueryPerformanceFrequency(&frequency);
-
-    for (int i = 0; i < 30; i++) {
-        QueryPerformanceCounter(&start);
-
-        x64calc(array_size, arr, ans64);
-
-        QueryPerformanceCounter(&end);
-
-        time_taken = ((double)(end.QuadPart - start.QuadPart) * 1000000.0) / frequency.QuadPart;
-        total_time += time_taken;
-    }
-
-    query_total = total_time;
-    query_avg = total_time / 30;
-
-    // Printing the results of the acceleration calculations
-    for (unsigned long long j = 0; j < array_size; j++) {
-        printf("[%llu] acceleration = %d\n", j, ans64[j]);
-    }
-
-    printf("\n----------------------------------------\n");
-    printf("\nRESULTS \n\n");
-    printf("Number of rows: %llu\n", array_size);
-    printf("Correctness check: \n");
-    cmpArr(array_size, ansC, ans64);
-
-    printf("\nQuery Performance\n");
-    printf("Average time per query (30 runs): %.2f 탎\n", query_avg);
-    printf("Total query time for 30 runs: %.2f 탎\n", query_total);
-
-    printf("\n----------------------------------------\n");
 }
 
 void dispMenu() {
     printf("Choose an option:\n");
     printf("[1] Insert an array\n");
-    printf("[2] Read 10.txt\n");
+    printf("[2] Read from 10.txt\n");
     printf("[3] Read from 100.txt\n");
     printf("[4] Read from 1000.txt\n");
     printf("[5] Read from 10000.txt\n");
@@ -220,13 +121,8 @@ void dispMenu() {
 }
 
 int main() {
-    //unsigned long long int array_size = 0;
-    //unsigned long long int array_bytes = 0;
-    //float (*arr)[3];
-    
-    /*clock_t start, end;
-    double time_taken, c_total, c_avg, x_total, x_avg;
-    double total_time = 0.0;*/
+    unsigned long long int array_size = 0;
+    float (*arr)[3] = NULL; 
     int choice;
 
     dispMenu();
@@ -237,76 +133,87 @@ int main() {
 
     switch (choice) {
     case 1:
-        getInput();
+        getInput(&array_size, &arr);
         break;
     case 2:
-        getTxt("10.txt");
+        getTxt("10.txt", &array_size, &arr);
         break;
     case 3:
-        getTxt("100.txt");
+        getTxt("100.txt", &array_size, &arr);
         break;
     case 4:
-        getTxt("1000.txt");
+        getTxt("1000.txt", &array_size, &arr);
         break;
     case 5:
-        getTxt("10000.txt");
+        getTxt("10000.txt", &array_size, &arr);
         break;
     default:
-        printf("Error\n");
+        printf("Error: Invalid choice.\n");
         return 1;
     }
-    
-    //array_bytes = array_size * 3 * sizeof(float);
-    /*float* ansC = (float*)malloc(array_size * sizeof(float));
-    int* ans64 = (int*)malloc(array_size * sizeof(float));*/
 
-    // first 5 rows check
-    /*for (unsigned long long i = 0; i < array_size && i < 5; i++) {
-        printf("[Row %llu] vi = %.2f | vf = %.2f | t = %.2f\n", i + 1, arr[i][0], arr[i][1], arr[i][2]);
+    float* ansC = malloc(array_size * sizeof(float));
+    int* ans64 = malloc(array_size * sizeof(int));
+
+    if (ansC == NULL || ans64 == NULL) {
+        printf("Error: Memory allocation failed for result arrays.\n");
+        free(arr);
+        exit(1);
     }
-    */
-    //float arr[][3] = { {0.0, 62.5, 10.1}, {60.0, 122.3, 5.5},{30.0, 160.7, 7.8} };
 
-    /*for (int i = 0; i < 30; i++) {
-        start = clock();
-        getAcceleration(array_size, arr, ansC);
-        end = clock();
-        time_taken = ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
-        total_time += time_taken;
-    }
-    c_total= total_time;
-    c_avg = total_time/30;
+    //https://stackoverflow.com/questions/1739259/how-to-use-queryperformancecounter
 
-    total_time = 0.0; //reset
+    LARGE_INTEGER start, end, freq;
+    double c_total = 0.0, time_taken, c_avg;
+    double x_total = 0.0, x_avg;
+
+    QueryPerformanceFrequency(&freq);
 
     for (int i = 0; i < 30; i++) {
-        start = clock();
-        x64calc(array_size, arr, ans64);
-        end = clock();
-        time_taken = ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
-        total_time += time_taken;
+        QueryPerformanceCounter(&start);
+
+        getAcceleration(array_size, arr, ansC);
+
+        QueryPerformanceCounter(&end);
+
+        time_taken = ((double)(end.QuadPart - start.QuadPart) * 1000000.0) / freq.QuadPart;
+        c_total += time_taken;
     }
-    x_total= total_time;
-    x_avg =total_time / 30;
-    
+    c_avg = c_total / 30;
+
+    for (int i = 0; i < 30; i++) {
+        QueryPerformanceCounter(&start);
+
+        x64calc(array_size, arr, ans64);
+
+        QueryPerformanceCounter(&end);
+
+        time_taken = ((double)(end.QuadPart - start.QuadPart) * 1000000.0) / freq.QuadPart;
+        x_total += time_taken;
+    }
+
+    x_avg = x_total / 30;
+
+    for (unsigned long long j = 0; j < array_size; j++) {
+        printf("[%llu] acceleration = %d\n", j, ans64[j]);
+    }
+
     printf("\n----------------------------------------\n");
-    printf("\nRESULTS \n");
+    printf("\nRESULTS \n\n");
     printf("Number of rows: %llu\n", array_size);
     printf("Correctness check: \n");
     cmpArr(array_size, ansC, ans64);
 
     printf("\nC Performance\n");
-    printf("Average time taken (30 runs): %.2f ms\n", c_avg);
-    printf("Total time taken   (30 runs): %.2f ms\n", c_total);
+    printf("Average time taken (30 runs): %.2f microseconds\n", c_avg);
+    printf("Total time taken (30 runs):   %.2f microseconds\n", c_total);
 
     printf("\nx86-64 Performance\n");
-    printf("Average time taken (30 runs): %.2f ms\n", x_avg);
-    printf("Total time taken   (30 runs): %.2f ms\n", x_total);
+    printf("Average time taken (30 runs): %.2f microseconds\n", x_avg);
+    printf("Total time taken (30 runs):   %.2f microseconds\n", x_total);
 
-    printf("\n----------------------------------------\n");*/
-    
-    //free(arr);
-    //free(ansC);
+    printf("\n----------------------------------------\n");
 
     return 0;
 }
+
